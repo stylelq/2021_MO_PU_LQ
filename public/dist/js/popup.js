2bc1fe418340eb9a1fa70ed2213441669edefb9c
DESKTOP-QD212U2\USER-PC <hyang@metabrain.com>
2021-11-12 13:46:35 +0900

    제품상세 확대축소 팝업 작업완료(panzoom 플러그인 사용)

--- public/dist/js/popup.js
+++ public/dist/js/popup.js
@@ -10,12 +10,62 @@ jQuery(function () {
     return false;
   }
 
-  $(document).on('click', '.js-popup-open', openPopup); //팝업닫기
+  $(document).on('click', '.js-popup-open', openPopup); //확대축소(플러그인 panzoom)
 
-  function closePopup() {
-    $('.popup, .small-popup').removeClass('is-active');
-    $('html').removeClass('is-hidden');
+  function productZoom() {
+    var area = document.querySelectorAll('.js-product-zoom');
+    var instance = "";
+
+    for (var i = 0; i < area.length; i++) {
+      var item = area.item(i);
+      instance = panzoom(item);
+    }
   }
 
-  $(document).on('click', '.js-popup-close, .small-popup__dim', closePopup);
+  if ($('.product-zoom').length > 0) {
+    productZoom();
+  } //확대축소(플러그인 panzoom):reset
+
+
+  function resetPanZoom() {
+    var area = document.querySelectorAll('.js-product-zoom');
+    var instance = "";
+
+    for (var i = 0; i < area.length; i++) {
+      var item = area.item(i);
+      instance = panzoom(item).zoomAbs(0, 0, 1);
+    }
+  } //zoom slider
+
+
+  if ($('.popup-slider').length > 0) {
+    var zoomSlide = new Swiper('.popup-slider__container', {
+      observer: true,
+      observeParents: true,
+      watchOverflow: true,
+      allowTouchMove: false,
+      slidesPerView: 1,
+      navigation: {
+        nextEl: ".popup-slider__button--next",
+        prevEl: ".popup-slider__button--prev"
+      },
+      on: {
+        slideChange: function slideChange() {
+          resetPanZoom();
+        }
+      }
+    });
+  } //팝업닫기
+
+
+  function closePopup() {
+    $('.popup, .small-popup, .slide-popup').removeClass('is-active');
+    $('html').removeClass('is-hidden');
+
+    if ($(this).hasClass('slide-popup__close')) {
+      resetPanZoom();
+    }
+  }
+
+  $(document).on('click', '.js-popup-close', closePopup);
 });
