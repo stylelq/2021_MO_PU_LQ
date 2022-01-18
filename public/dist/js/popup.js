3a250c838bbb51cf4a18830a390589697406b071
mj-han <tarot0312@gmail.com>
2022-01-18 18:43:51 +0900

    re

--- public/dist/js/popup.js
+++ public/dist/js/popup.js
@@ -57,7 +57,7 @@ jQuery(function () {
   $(document).on('click', function (e) {
     // 예외처리
     // [개발기준작업] 주문서-배송지정보, 1:1문의-주문검색 팝업
-    if (e.target.tagName === 'INPUT' || e.target.tagName === 'BUTTON' || e.target.tagName === 'DIV') {
+    if (e.target.tagName === 'A' || e.target.tagName === 'BUTTON' || e.target.tagName === 'DIV') {
       if ($('.popup.page-delivery').hasClass('is-active') || $('#inquiryOrder').length > 0) {
         $('html').addClass('is-hidden');
       }
