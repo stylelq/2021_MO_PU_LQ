c2b568fd3c9d9102606387c9e31a22beb1603119
sb_lee <lsbbom@gmail.com>
2022-03-02 14:26:09 +0900

    메인 배너옵션 스타일 수정

--- public/src/js/common.js
+++ public/src/js/common.js
@@ -765,21 +765,28 @@ jQuery(function(){
     //     });
     // }
 
-
     // 메인 배너슬라이드
-    if($('.main-banner').length > 0 && $('.main-banner__item').length > 1 ){ 
+    if($('.main-banner').length > 0 && $('.main-banner__item').length > 1 ){
         var mainBannerProgressbarOpt= {
             init: function () {
                 var slide = $(this.$wrapperEl[0]).find(".swiper-slide-active");
                 var bg = slide.data("bg");
-                if ($('.main-banner__item[data-bg="white"]').hasClass('swiper-slide-active')) {
+                /*if ($('.main-banner__item[data-bg="white"]').hasClass('swiper-slide-active')) {
                     $('body').removeClass('is-black');
                     $('body').addClass('is-white');
                 } else {
                     $('body').removeClass('is-white');
                     $('body').addClass('is-black');
+                }*/
+
+                if($('body').hasClass('is-black')) {
+                    $('.main-banner__pagination').removeClass('is-black');
+                    $('.main-banner__pagination').addClass('is-white');
+                }else if($('body').hasClass('is-white')){
+                    $('.main-banner__pagination').removeClass('is-white');
+                    $('.main-banner__pagination').addClass('is-black');
                 }
-    
+
                 $('.main-banner__progressbar').removeClass("animate");
                 $('.main-banner__progressbar').removeClass("active");
                 $('.main-banner__progressbar').eq(0).addClass("animate");
