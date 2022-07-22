e017f85613376212f99aabc17884f3546c1343fa
sb_lee <lsbbom@gmail.com>
2022-07-22 18:14:40 +0900

    fyl3 리스트페이지 퍼블리싱 진행중

---
+++ public/dist/js/fyl.js
@@ -0,0 +1,11 @@
+"use strict";
+
+jQuery(function () {
+  $(document).ready(function () {// 영상재생 및 정지
+
+    /*function videoPlay(){
+        $('jsPlayVideo').pause();
+    }
+    $(document).on('click', '.jsPlayButton', videoPlay);*/
+  });
+});
---
+++ public/src/js/fyl.js
@@ -0,0 +1,9 @@
+jQuery(function () {
+    $(document).ready(function () {
+        // 영상재생 및 정지
+        /*function videoPlay(){
+            $('jsPlayVideo').pause();
+        }
+        $(document).on('click', '.jsPlayButton', videoPlay);*/
+    });
+});
